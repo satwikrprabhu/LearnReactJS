91bcc065bb4d27ce4caca0cafea7c30cab716eca
satwikrprabhu <satwikprabhu@gmail.com>
2023-02-24 22:59:37 +0530

    Intro to React

---
+++ textutils/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+let name = "Satwik";
+function App() {
+  return (
+   <>
+   <nav>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+   </nav>
+   <div className="container">
+    <h1>Hello {name}</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae nobis illo autem pariatur quod corporis, praesentium non accusamus expedita, aperiam delectus magnam labore aut nisi ex nemo quo sed maiores. Obcaecati quis aut provident.</p>
+   </div>
+   </>
+  );
+}
+
+export default App;
