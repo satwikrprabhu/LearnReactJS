5b1a962186943db26dca623b130da1270da80cc4
satwikrprabhu <satwikprabhu@gmail.com>
2023-02-27 23:22:26 +0530

    Added filler text

--- AirBNB Clone/components/MainContent.js
+++ AirBNB Clone/components/MainContent.js
@@ -4,6 +4,8 @@ export default function MainContent(){
     return(
     <div>
     <img src="../images/photo-grid.png" width="100%" className="midtile"/>
+    <h1 className="comp">Online Experiences</h1>
+    <p className="comppara">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
     </div>
     )
 }
