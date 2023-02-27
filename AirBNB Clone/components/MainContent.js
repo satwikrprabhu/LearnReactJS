67dc23f00f480ba0fd5826196a7b27657b2afabd
satwikrprabhu <satwikprabhu@gmail.com>
2023-02-27 23:09:23 +0530

    Added components

---
+++ AirBNB Clone/components/MainContent.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function MainContent(){
+    return(
+    <div>
+    <img src="../images/photo-grid.png" width="100%" className="midtile"/>
+    </div>
+    )
+}
